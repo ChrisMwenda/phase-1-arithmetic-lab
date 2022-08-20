361c664d3252a5389d0695cd80d92040713bdf24
Christopher <chrismbogo97@gmail.com>
2022-08-20 14:42:27 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+let num1=31,num2=2;
+let multiply=num1*num2
 
-// Write your code here
+let random=Math.floor((Math.random()*10));
+console.log(Number.isInteger(random));
+
+let num3=20,num4=8;
+let mod=num3%num4;
+
+let max=Math.max(10,13,17,11,9,20);
